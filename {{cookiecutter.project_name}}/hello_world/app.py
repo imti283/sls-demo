77f7e6496f9be25ff73cfc42403ed696fab28b42
Md Imtiyaz Alam <md.imtiyaz.alam@hp.com>
2020-07-01 12:53:21 +0530

    changes in code

--- {{cookiecutter.project_name}}/hello_world/app.py
+++ {{cookiecutter.project_name}}/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello to HP world",
             # "location": ip.text.replace("\n", "")
         }),
     }
